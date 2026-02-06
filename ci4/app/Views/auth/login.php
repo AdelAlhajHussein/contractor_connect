07c58e0af2156e797ecdb39a66aae1756f087278
AdelAlhajHussein <Adel.AlhajHussein@georgebrown.ca>
2026-02-06 08:39:32 -0500

    Commit Login View 2.1

--- ci4/app/Views/auth/login.php
+++ ci4/app/Views/auth/login.php
@@ -1,10 +1,30 @@
 <!doctype html>
-<html>
+<html lang="en">
 <head>
     <meta charset="utf-8">
     <title>Login</title>
 </head>
 <body>
-<h1>Login Page ✅</h1>
+
+<h1>Login</h1>
+
+<form method="post" action="<?= site_url('login') ?>">
+    <?= csrf_field() ?>
+
+    <div>
+        <label>Username</label><br>
+        <input type="text" name="username" required>
+    </div>
+
+    <div style="margin-top:10px;">
+        <label>Password</label><br>
+        <input type="password" name="password" required>
+    </div>
+
+    <div style="margin-top:15px;">
+        <button type="submit">Login</button>
+    </div>
+</form>
+
 </body>
 </html>
