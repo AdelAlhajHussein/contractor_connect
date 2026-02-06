578cf63eaca2275a6e3c94e94ce500559e9a2821
eric-laudrum <ericlaudrum@gmail.com>
2026-02-05 20:42:11 -0500

    Add basic layout for main page

---
+++ ci4/app/Views/layouts/main.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+
+    <!-- header -->
+
+    <!-- nav-bar -->
+
+    <!-- sections -->
+
+    <!-- footer -->
+
+
+</body>
+</html>
