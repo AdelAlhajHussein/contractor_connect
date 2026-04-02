5bcfe6bfdd756f7b17575c8b04f26a5433c38583
eric-laudrum <ericlaudrum@gmail.com>
2026-04-02 12:49:24 -0400

    add: initial RatingsControllerTest.php file

---
+++ ci4/tests/Feature/Admin/RatingsControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature\Admin;
+
+use CodeIgniter\Test\DatabaseTestTrait;
+use CodeIgniter\Test\FeatureTestTrait;
+use CodeIgniter\Test\CIUnitTestCase;
+
+final class RatingsControllerTest extends CIUnitTestCase
+{
+    use DatabaseTestTrait, FeatureTestTrait;
+
+    protected $refresh = true;
+    protected $namespace = 'App';
+
+
+
+
+}
