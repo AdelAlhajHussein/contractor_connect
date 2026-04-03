f77497fc2b13260092444885666fa0fb0a302101
eric-laudrum <ericlaudrum@gmail.com>
2026-04-02 22:21:30 -0400

    add: contractor controller migrations and create initial test file

---
+++ ci4/app/Database/Migrations/2026-04-03-020728_CreateContractorProfiles.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateContractorProfiles extends Migration
+{
+    public function up()
+    {
+        //
+        $this->forge->addField([
+            'id'=>[
+                'type'=>'BIGINT',
+                'constraint'=>20,
+                'unsigned'=>true,
+                'auto_increment'=>true
+            ],
+            'contractor_id'=>[
+                'type'=>'BIGINT',
+                'constraint'=>20,
+                'unsigned'=>true,
+            ],
+            'address'=>[
+                'type'=>'VARCHAR',
+                'constraint'=>255,
+                'null'=>true,
+            ],
+            'province'=>[
+                'type'=>'VARCHAR',
+                'constraint'=>100,
+                'null'=>false,
+            ],
+            'postal_code'=>[
+                'type'=>'VARCHAR',
+                'constraint'=>20,
+                'null'=>true,
+            ],
+            'approval_status'=>[
+                'type'=>'VARCHAR',
+                'constraint'=>20,
+                'default' => 'pending',
+                'null'=>true,
+            ],
+            'created_at'=>[
+                'type'=>'DATETIME',
+                'null'=>true,
+            ],
+            'updated_at'=>[
+                'type'=>'DATETIME',
+                'null'=>true,
+            ],
+        ]);
+
+        $this->forge->addKey('id',true);
+        $this->forge->addForeignKey('contractor_id', 'users', 'id', 'CASCADE', 'CASCADE');
+        $this->forge->createTable('contractor_profiles');
+    }
+
+    public function down()
+    {
+        //
+        $this->forge->dropTable('contractor_profiles');
+    }
+}
---
+++ ci4/tests/Feature/Admin/ContractorsControllerTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature\Admin;
+
+use CodeIgniter\Test\CIUnitTestCase;
+use CodeIgniter\Test\FeatureTestTrait;
+use CodeIgniter\Test\DatabaseTestTrait;
+
+class ContractorsControllerTest extends CIUnitTestCase
+{
+    use FeatureTestTrait;
+    use DatabaseTestTrait;
+
+    protected $refresh = true;
+    protected $namespace = 'App';
+    protected $migrate = true;
+
+
+
+
+}
+
+
+
+
