dba451f7557a4584aaeb3a366f0fe372ad443122
eric-laudrum <ericlaudrum@gmail.com>
2026-04-02 22:30:51 -0400

    add: helper function to create contractors for tests

--- ci4/tests/Feature/Admin/ContractorsControllerTest.php
+++ ci4/tests/Feature/Admin/ContractorsControllerTest.php
@@ -15,7 +15,33 @@ class ContractorsControllerTest extends CIUnitTestCase
     protected $namespace = 'App';
     protected $migrate = true;
 
+    // Helper
+    private function createContractor(array $userData = [], array $profileData = []){
+        $userDefaults = [
+            'username'=>'test_contractor',
+            'email'=>'contractor@test.com',
+            'first_name'=>'Test',
+            'last_name'=>'Contractor',
+            'role_id' => 2,
+            'is_active'=>1,
+            'password_hash'=>'fake_hash'
+        ];
 
+        $userId = $this->db->table('users')->insert(array_merge($userDefaults,$userData));
+
+        $profileDefaults = [
+            'contractor_id'=>$userId,
+            'city'=>'Toronto',
+            'province'=>'ON',
+            'approval_status'=>'pending',
+        ];
+
+        $this->db->table('contractor_profiles')->insert(array_merge($profileDefaults,$profileData));
+
+        return $userId;
+    }
+
+    // Tests
 
 
 }
