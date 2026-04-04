9184b153e6fc8a855f708a8fd4f1921a018165c8
eric-laudrum <ericlaudrum@gmail.com>
2026-04-03 20:40:49 -0400

    create: file for helper functions in _support

---
+++ ci4/tests/_support/ProjectTestCase.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tests\Support;
+
+use CodeIgniter\Test\CIUnitTestCase;
+use CodeIgniter\Test\FeatureTestTrait;
+use CodeIgniter\Test\DatabaseTestTrait;
+
+abstract class ProjectTestCase extends CIUnitTestCase{
+    use FeatureTestTrait, DatabaseTestTrait;
+
+    // Standard configuration
+    protected $namespace = 'App';
+    protected $refresh = true;
+    protected $migrate = true;
+
+
+    protected function setUpCategory(array $data = []){
+        $defaults = ['name' => 'General'];
+        return $this->db->table('categories')
+            ->insert(array_merge($defaults, $data));
+    }
+
+    protected function setUpUser(array $data = []){
+        $defaults = [
+        'username' => 'user_' . uniqid(),
+        'email' => uniqid() . '@test.com',
+        'first_name' => 'First',
+        'last_name' => 'Last',
+        'role_id' => 3,
+        'is_active' => 1,
+        'password_hash' => 'fake_hash'
+        ];
+
+        return $this->db->table('users')
+            ->insert(array_merge($defaults, $data));
+    }
+
+    protected function setUpProject(array $data = []){
+        // Resolve dependencies
+        $categoryId  = $data['category_id'] ?? $this->setUpCategory();
+        $homeownerId = $data['home_owner_id'] ?? $this->setUpUser(['role_id' => 2]);
+
+        $defaults = [
+            'title' => 'Test Project',
+            'description' => 'Test description',
+            'address' => '123 Project St.',
+            'status' => 'open',
+            'category_id' => $categoryId,
+            'home_owner_id' => $homeownerId,
+        ];
+
+        return $this->db->table('projects')
+            ->insert(array_merge($defaults, $data));
+    }
+}
