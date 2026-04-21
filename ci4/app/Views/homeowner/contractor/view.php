c846f9e1646c9879941563109f55839f70326ca6
shifa <shifa.karnelia@georgebrown.ca>
2026-04-21 19:02:09 -0400

    chnage into view.php homonwer/contractor

--- ci4/app/Views/homeowner/contractor/view.php
+++ ci4/app/Views/homeowner/contractor/view.php
@@ -1,15 +1,14 @@
 <?= $this->extend('layouts/main') ?>
 
+<?= $this->section('page_css') ?>
+    <link rel="stylesheet" href="<?= base_url('css/admin-index.css') ?>">
+<?= $this->endSection() ?>
+
 <?= $this->section('content') ?>
 
     <div class="users-container">
 
-        <h1>Contractor Profile</h1>
-
-        <p>
-            <strong>Name:</strong>
-            <?= esc($contractor['username']) ?>
-        </p>
+        <h1 class="users-title">Contractor Profile</h1>
 
         <p>
             <strong>Email:</strong>
@@ -17,13 +16,23 @@
         </p>
 
 
-        <a href="<?= site_url('homeowner/bids') ?>"
-           class="btn btn-secondary">
 
-            Back to Bids
+
+
+        <!-- CONNECT BUTTON (opens email app) -->
+        <div class="btn-group">
+            <a class="btn btn-primary"
+               href="mailto:<?= esc($contractor['email']) ?>?subject= Project Inquiry from contractor connect website ">
+                Connect via Email
+            </a>
+        </div>
+
+        <a  class="btn btn-outline-danger" href="<?= site_url('homeowner/bids') ?>">
+
+            <-- Back to Bids
 
         </a>
-
     </div>
+
 
 <?= $this->endSection() ?>
